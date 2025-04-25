555f8edc37ee6f62a9048ac4da020bc716671d6b
durgesh <durgesh.acharya2006@gmail.com>
2025-04-26 00:16:13 +0530

    config changed

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,11 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'picsum.photos',
-      }
+      },
+      {
+        protocol: 'http',              // 👈 use 'http' here since your domain is not using https
+        hostname: '103.168.18.92',     // 👈 your VPS IP or domain name
+      },
     ]
   },
 };
