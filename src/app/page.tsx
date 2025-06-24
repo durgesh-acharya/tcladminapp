38d262149cec3c5084bdbc65b5c0d0719776d0c2
durgesh <durgesh.acharya2006@gmail.com>
2025-06-24 14:09:05 +0530

    changes made in deployement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <div className="flex items-center justify-center h-screen">
       <h1 className="text-3xl font-extrabold text-center">
-        Travelers Clan Admin Dashboard
+        Travelers Clan Admin
       </h1>
     </div>
   );
